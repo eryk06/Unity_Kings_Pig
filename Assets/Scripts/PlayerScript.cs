466836a8a2225fae8078b711d4b8e46aee6ec585
Erykdev06 <lamnguyen062003@gmail.com>
2023-06-02 23:25:31 +0700

    update file

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,82 +1,81 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    private bool isALive;
-    private float speed;
-    public bool isNen;
-    private new Rigidbody2D rigidbody2D;
-    public bool isRight;
+  private bool isALive;
+  private float speed;
+  public bool isNen;
+  private new Rigidbody2D rigidbody2D;
+  public bool isRight;
 
-    // animator
-    private Animator animator;
-    // Start is called before the first frame update
-    void Start()
+  // animator
+  private Animator animator;
+  // Start is called before the first frame update
+  void Start()
+  {
+    isALive = true;
+    rigidbody2D = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    animator.SetBool("isRunning", false);
+    animator.SetBool("isJump", false);
+
+    Vector2 scale = transform.localScale;
+
+    if (Input.GetKeyDown(KeyCode.Space))
     {
-        isALive = true;
-        rigidbody2D = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-
+      var x = transform.position.x + (isRight ? 0.5f : -0.5f);
+      var y = transform.position.y;
+      var z = transform.position.z;
     }
 
-    // Update is called once per frame
-    void Update()
+    if (Input.GetKey(KeyCode.RightArrow))
     {
-        animator.SetBool("isRunning", false);
-        animator.SetBool("isJump", false);
-
-        Vector2 scale = transform.localScale;
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            var x = transform.position.x + (isRight ? 0.5f : -0.5f);
-            var y = transform.position.y;
-            var z = transform.position.z;
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            if (isRight == false)
-            {
-                scale.x *= scale.x > 0 ? 1 : -1;
-                transform.localScale = scale;
-                isRight = true;
-            }
-            animator.SetBool("isRunning", true);
-            scale.x = 8;
-            transform.Translate(Vector3.right * 15f * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            if (isRight == true)
-            {
-                scale.x *= scale.x > 0 ? -1 : 1;
-                transform.localScale = scale;
-                isRight = false;
-            }
-            animator.SetBool("isRunning", true);
-            scale.x = -8;
-            transform.Translate(Vector3.left * 15f * Time.deltaTime);
-        }
-
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            rigidbody2D.AddForce(new Vector2(0, 560));
-            isNen = false;
-        }
-        if (!isNen)
-        {
-            animator.SetBool("isJump", true);
-        }
+      if (isRight == false)
+      {
+        scale.x *= scale.x > 0 ? 1 : -1;
+        transform.localScale = scale;
+        isRight = true;
+      }
+      animator.SetBool("isRunning", true);
+      scale.x = 8;
+      transform.Translate(Vector3.right * 15f * Time.deltaTime);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("San"))
-        {
-            isNen = true;
-        }
+    if (Input.GetKey(KeyCode.LeftArrow))
+    {
+      if (isRight == true)
+      {
+        scale.x *= scale.x > 0 ? -1 : 1;
+        transform.localScale = scale;
+        isRight = false;
+      }
+      animator.SetBool("isRunning", true);
+      scale.x = -8;
+      transform.Translate(Vector3.left * 15f * Time.deltaTime);
     }
+
+    if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+      rigidbody2D.AddForce(new Vector2(0, 560));
+      isNen = false;
+    }
+    if (!isNen)
+    {
+      animator.SetBool("isJump", true);
+    }
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.CompareTag("San"))
+    {
+      isNen = true;
+    }
+  }
 }
